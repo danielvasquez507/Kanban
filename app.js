db504902cb156b9564b9d1f049b04ef246ebd749
Daniel <dany@LinuxU-DV.home.lla.com>
2026-07-23 19:10:40 -0500

    fix: bump JS version string

--- app.js
+++ app.js
@@ -1054,6 +1054,6 @@ function moveSubtask(id, tIdx, stIdx, dir) {
   renderTasks(id);
 }
 
-const APP_VERSION = 'v0.1.52';
+const APP_VERSION = 'v0.1.53';
 const versionLabel = document.getElementById('appVersionLabel');
 if (versionLabel) versionLabel.textContent = APP_VERSION;
